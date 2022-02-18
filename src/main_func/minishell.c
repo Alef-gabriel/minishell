30589a92e01ed85bf34c0bc940f72ed4add4372b
Anderson Porto <andersonhigo2@gmail.com>
2022-02-18 01:11:21 -0300

    add: rl_clear_history

--- src/main_func/minishell.c
+++ src/main_func/minishell.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 00:57:51 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/02/18 00:09:25 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2022/02/18 00:55:02 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,17 @@ void	shell_loop(void)
 		get_directory();
 		get_sig();
 		input = takeinput();
-		s = ft_strtrim(input, " ");
-		if (s[0] != '\0')
-			parse_input(s);
-		free(s);
-		free(input);
+		parse_input(s);
 		g_mini.pipes = 0;
 	}
 }
 
-// verificar >>> init_term() mensagem inicial;
 int	main(int argc __attribute__((unused)),
 		char **argv __attribute__((unused)), char **envp)
 {
 	init_vars();
 	g_mini.env_table = env_to_hash(envp);
 	shell_loop();
+	rl_clear_history();
 	return (0);
 }
