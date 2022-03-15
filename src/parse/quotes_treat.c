99af61ed95b3e10013a4cd6ba08d791fd855f82f
Anderson Porto <andersonhigo2@gmail.com>
2022-03-15 03:26:34 -0300

    style: english words

--- src/parse/quotes_treat.c
+++ src/parse/quotes_treat.c
@@ -16,17 +16,17 @@ char	*join_matrix(char **split)
 	return (join);
 }
 
-static int	quote_expacion_verify(char *quote, int expancion)
+static int	quote_expansion_verify(char *quote, int expansion)
 {
 	int	quote_value;
 
 	quote_value = *quote;
-	if (expancion == -1)
+	if (expansion == -1)
 	{
 		*quote = 1;
 		return (quote_value);
 	}
-	else if (expancion == quote_value)
+	else if (expansion == quote_value)
 		*quote = 1;
 	return (-1);
 }
@@ -42,7 +42,7 @@ char	*treat_quotes(char *str)
 	while (str[i])
 	{
 		if (str[i] == '\'' || str[i] == '\"')
-			u = quote_expacion_verify(str + i, u);
+			u = quote_expansion_verify(str + i, u);
 		else if (u == -1 && str[i] == ' ')
 			str[i] = SPACECHAR;
 		else if (u == -1 && str[i] == '|')
