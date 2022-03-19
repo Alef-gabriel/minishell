3cdd0d337bd5a3c326c8a22db6cab3e47ace8892
Anderson Porto <andersonhigo2@gmail.com>
2022-03-19 00:04:38 -0300

    fix: heredoc signal

--- src/main/signal_heredoc.c
+++ src/main/signal_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 23:17:13 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/03/19 00:02:12 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2022/03/19 00:03:34 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,8 @@ void	get_sig_heredoc(void)
 static void	abort_sig_heredoc(int signal __attribute__((unused)))
 {
 	ft_putchar_fd('\n', STDERR_FILENO);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 	g_mini.exit_code = 130 + signal;
 }
